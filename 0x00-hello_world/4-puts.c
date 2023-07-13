8e9346329957b3c090f0db1d8acdfa92e1be3d99
Kobby Fynn <kobbyfynn14@gmai.com>
2023-07-13 07:23:19 -0700

    complete task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - program entry
+ *
+ * Return: 0 upon success
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
