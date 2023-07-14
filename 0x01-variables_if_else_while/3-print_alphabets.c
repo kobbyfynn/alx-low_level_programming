e29fa4979d8eee1b8e27f2e0ad9fff47994e676b
Kobby Fynn <kobbyfynn14@gmai.com>
2023-07-14 09:55:25 -0700

    complete task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Program entry
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char i;
+
+	i = 'a';
+	while (i <= 'z')
+	{
+		putchar(i);
+		i++;
+	}
+	i = 'A';
+	while (i <= 'Z')
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
