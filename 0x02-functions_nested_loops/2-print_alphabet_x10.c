53adbfc76ba0af0bb77e4147e2d0d039ed55257e
Kobby Fynn <kobbyfynn14@gmai.com>
2023-07-18 11:42:35 -0700

    complete task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - function that prints 10 times the alphabet
+ * in lowercase.
+ *
+ * Return: void.
+ */
+void print_alphabet_x10(void)
+{
+	int j;
+	char i;
+
+	for (j = 0; j < 10; j++)
+	{
+		for (i = 'a'; i <= 'z'; i++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
+	}
+}
