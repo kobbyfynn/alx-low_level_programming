24e856ba9d83f4a50616a02e6e7c3e70a0b87afc
Kobby Fynn <kobbyfynn14@gmai.com>
2023-07-20 18:46:04 -0700

    complete task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -26,7 +26,14 @@ int main(void)
 			}
 			if (i % 5 == 0)
 			{
-				printf("Buzz ");
+				if (i == 100)
+				{
+					printf("Buzz");
+				}
+				else
+				{
+					printf("Buzz ");
+				}
 			}
 		}
 		if (!(i % 3 == 0 || i % 5 == 0))
