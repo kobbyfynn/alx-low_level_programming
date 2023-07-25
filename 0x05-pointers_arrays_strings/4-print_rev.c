372d319eecf5d248eeb371b02867931b43120df4
Kobby Fynn <kobbyfynn14@gmai.com>
2023-07-24 17:42:36 -0700

    complete task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_rev -  function that prints a string, in reverse,
+ * followed by a new line.
+ *
+ * @s: string to be printed
+ */
+
+void print_rev(char *s)
+{
+	int i;
+
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		continue;
+	}
+
+	for (i = i - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+
+	_putchar('\n');
+}
