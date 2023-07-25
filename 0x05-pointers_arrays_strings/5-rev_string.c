82238d342ce310855198ffdf28e4d674fdfa592d
Kobby Fynn <kobbyfynn14@gmai.com>
2023-07-24 18:47:22 -0700

    complete task 6

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * rev_string - function that reverses a string.
+ *
+ * @s: string
+*/
+
+void rev_string(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		continue;
+	}
+
+	for (i = i - 1; i >= 0; i--)
+	{
+	}
+}
