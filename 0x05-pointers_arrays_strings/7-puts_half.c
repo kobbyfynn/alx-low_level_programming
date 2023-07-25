cd38308474a36275c240f7690715a67ea522a8c9
Kobby Fynn <kobbyfynn14@gmai.com>
2023-07-25 06:43:58 -0700

    complete task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,7 +1,31 @@
 #include "main.h"
 
-int main(void)
+/**
+ * puts_half - function that prints half of a string, followed by a new line.
+ *
+ * @str: string
+*/
+
+void puts_half(char *str)
 {
-	printf("0123456789\n");
-	return (0);
+	int len, n;
+
+	for (len = 0; str[len] != '\0'; len++)
+	{
+		if (len % 2 != 0)
+		{
+			n = (len - 1) / 2;
+		}
+		else
+		{
+			n = len / 2;
+		}
+	}
+
+	for (len = n + 1; str[len] != '\0'; len++)
+	{
+		_putchar(str[len]);
+	}
+
+	_putchar('\n');
 }
