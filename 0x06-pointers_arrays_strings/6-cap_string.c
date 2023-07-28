d5765334bce0fb187acc9d2e0aec2a4304e85fcc
Kobby Fynn <kobbyfynn14@gmai.com>
2023-07-28 16:41:07 -0700

    complete task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,7 +1,46 @@
 #include "main.h"
 
-int main(void)
+/**
+ * cap_string -  function that capitalizes all words of a string.
+ *
+ * @s: string
+ *
+ * Return: string
+*/
+
+char *cap_string(char *s)
 {
-	printf("0123456789\n");
-	return (0);
+	int j, i = 0;
+	/* j represents whether or not the current character is a start of a new
+	 * word */
+
+	for (; s[i] != '\0'; i++)
+	{
+		if (s[i] == '\n' || s[i] == ' ' || s[i] == '\t' || s[i] == '\v')
+		{
+			j = 1;
+		}
+		else if (s[i] == ',' || s[i] == ';' || s[i] == '.' || s[i] == '!')
+		{
+			j = 1;
+		}
+		else if (s[i] == '?' || s[i] == '"' || s[i] == '(' || s[i] == ')')
+		{
+			j = 1;
+		}
+		else if (s[i] == '{' || s[i] == '}')
+		{
+			j = 1;
+		}
+		else if (j && s[i] >= 'a' && s[i] <= 'z')
+		{
+			s[i] = s[i] - ('a' - 'A');
+			j = 0;
+		}
+		else
+		{
+			j = 0;
+		}
+	}
+	return (s);
 }
