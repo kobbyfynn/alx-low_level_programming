6db96f6647812d977f05f9c21f3a4bb741cfd8bc
Kobby Fynn <kobbyfynn14@gmai.com>
2023-07-28 18:13:31 -0700

    complete task 100

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,7 +1,30 @@
 #include "main.h"
 
-int main(void)
+/**
+ * rot13 - function that encodes a string using rot13.
+ *
+ * @s: string
+ *
+ * Return: address of string
+*/
+
+char *rot13(char *s)
 {
-	printf("0123456789\n");
-	return (0);
+	int i, j;
+	char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char b[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j <= 51; j++)
+		{
+			if (a[j] == s[i])
+			{
+				s[i] = b[j];
+				break;
+			}
+		}
+	}
+
+	return (s);
 }
