e120e58dafc01542b88718d2157309c7be44a11c
Kobby Fynn <kobbyfynn14@gmai.com>
2023-07-28 17:33:38 -0700

    complete task 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,7 +1,29 @@
 #include "main.h"
 
-int main(void)
+/**
+ * leet - function that encodes a string into 1337.
+ *
+ * @s: string
+ *
+ * Return: address of string
+*/
+
+char *leet(char *s)
 {
-	printf("0123456789\n");
-	return (0);
+	int j, i;
+	char a[] = "aAeEoOtTlL";
+	char b[] = "4433007711";
+
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			if (a[j] == s[i])
+			{
+				s[i] = b[j];
+			}
+		}
+	}
+	return (s);
 }
