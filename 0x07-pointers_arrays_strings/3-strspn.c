2653403c1aef30142106dd0bd018d39efd66e7c8
Kobby Fynn <kobbyfynn14@gmai.com>
2023-08-01 16:29:12 -0700

    recode task 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -15,26 +15,48 @@ unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int i;
 	unsigned int j;
-	unsigned int found;
+	unsigned int sum = 0;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		found = 0;
-
 		for (j = 0; accept[j] != '\0'; j++)
 		{
 			if (accept[j] == s[i])
 			{
-				found = 1;
+				sum += 1;
 				break;
 			}
 		}
-
-		if (!found)
+		if (s[i] != accept[j])
 		{
 			break;
 		}
 	}
 
-	return (i);
+	return (sum);
 }
+/*unsigned int i;
+  unsigned int j;
+  unsigned int found;
+
+  for (i = 0; s[i] != '\0'; i++)
+  {
+  found = 0;
+
+  for (j = 0; accept[j] != '\0'; j++)
+  {
+  if (accept[j] == s[i])
+  {
+  found = 1;
+  break;
+  }
+  }
+
+  if (!found)
+  {
+  break;
+  }
+  }
+
+  return (i);*/
+
