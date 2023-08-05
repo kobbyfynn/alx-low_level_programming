0d46bd9d252fdf6c62a461813789d439256355f5
Kobby Fynn <kobbyfynn14@gmai.com>
2023-08-05 15:55:41 -0700

    redo task 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -28,16 +28,15 @@ int main(int argc, char *argv[])
 		{
 			for (j = 0; argv[i][j] != '\0'; j++)
 			{
-				if (isdigit(argv[i][j]))
-				{
-					add += atoi(&argv[i][j]);
-				}
-				else
+				if (!isdigit(argv[i][j]))
 				{
 					printf("Error\n");
-					return (1);
+					{
+						return (1);
+					}
 				}
 			}
+			add += atoi(argv[i]);
 		}
 		printf("%d\n", add);
 	}
